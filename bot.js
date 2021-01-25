674771e5e3ab18243c5fccb3f2ee27bf06a61646
duckyfy <76946478+duckyfy@users.noreply.github.com>
2021-01-25 10:09:24 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,21 +18,20 @@ client.on('message', message => {
 
        message.reply('pong');
 
-       }	else if (message.content === `${prefix}test`) {
+       }	else if (message.content === `-test`) {
         message.channel.send('I am operational.');
         
-} else if (command === 'avatar') {
+}else if (message.content === '-avatar') {
         if (!message.mentions.users.size) {
             return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
-const avatarList = message.mentions.users.map(user => {
-            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
-        });
         
          
          //normal stuff
            } else if (message.content === `${prefix}evorsio`) {
         message.channel.send('**Evorsio is an upcoming sanbox**');
-        
+        } else if (message.content === `-server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+         
 });
 
  
